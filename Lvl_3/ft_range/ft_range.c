1fe3c77e899404e0fc5bd942548f51043537410c
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-06-24 09:23:29 +0100

    Update ft_range.c

--- Lvl_3/ft_range/ft_range.c
+++ Lvl_3/ft_range/ft_range.c
@@ -25,3 +25,20 @@ int	*ft_range(int start, int end)
 	}
 	return (arr);
 }
+
+//OR
+/*
+int	*ft_range(int start, int end)
+{
+	int range = abs(end - start);
+	int *arr = malloc(sizeof(int) * range);
+	int i = 0;
+
+	while (i < range)
+	{
+		arr[i++] = ((end > start) ? start++ : start--);
+	}
+	arr[i] = start;
+	return (arr);
+}
+*/
