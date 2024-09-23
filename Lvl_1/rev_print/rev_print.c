a4043a91d6daf030be59fc71a7805e543c0f8bbf
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:26:34 +0100

    Update rev_print.c

--- Lvl_1/rev_print/rev_print.c
+++ Lvl_1/rev_print/rev_print.c
@@ -1,5 +1,19 @@
 #include <unistd.h>
 
+/*
+	rev_print
+
+	This program takes a string as an argument and prints it in reverse order.
+
+	- We check if the number of arguments (`ac`) is 2, meaning the program is called with one argument.
+	- If valid:
+		- We initialize an index `i` to 0.
+		- The first loop increments `i` while the string `av[1][i]` is not null, effectively calculating the length of the string.
+		- The second loop prints the characters of the string in reverse order by decrementing `i`.
+			- In each iteration of the second loop, the character at `av[1][i]` is written to the standard output.
+	- After all characters have been printed in reverse, print a newline (`'\n'`).
+	- Finally, return `0` to indicate successful execution.
+*/
 int main (int ac, char **av)
 {
 	if (ac == 2)
