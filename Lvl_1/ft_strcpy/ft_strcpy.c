6d2def95c3bc34853b334eff3aca8a8122bfe4ec
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:22:44 +0100

    Update ft_strcpy.c

--- Lvl_1/ft_strcpy/ft_strcpy.c
+++ Lvl_1/ft_strcpy/ft_strcpy.c
@@ -1,6 +1,19 @@
-
 #include <stdio.h>
 
+/*
+	ft_strcpy
+
+	This function copies the content of the source string (s2) to the destination string (s1),
+	and returns the destination string.
+
+	- We initialize an index `i` to 0 to track our position in both strings.
+	- While the current character of the source string (s2) is not null:
+		- Copy the character from the source string (s2) to the destination string (s1);
+		- Move to the next character by incrementing `i`.
+	- Once we've reached the end of the source string (s2), we add a null-terminating character 
+	  to the destination string (s1) to properly terminate it.
+	- Finally, return the destination string (s1).
+*/
 char	*ft_strcpy(char *s1, char *s2)
 {
 	int i = 0;
