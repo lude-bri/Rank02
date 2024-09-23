3b78f78d8ad8584ff02c511d93e18010f4b66ffb
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:30:50 +0100

    Update ulstr.c

--- Lvl_1/ulstr/ulstr.c
+++ Lvl_1/ulstr/ulstr.c
@@ -1,5 +1,22 @@
 #include <unistd.h>
 
+/*
+	ulstr
+
+	This program takes a string as an argument and toggles the case of each letter.
+	If a letter is lowercase, it is converted to uppercase, and if it is uppercase, it is converted to lowercase.
+
+	- We check if the number of arguments (`ac`) is 2, meaning the program is called with one argument.
+	- If valid:
+		- We initialize an index `i` to 0 to iterate through the string (`av[1]`).
+		- The loop runs while the current character in the string (`av[1][i]`) is not null:
+			- If the character is a lowercase letter ('a' to 'z'), subtract 32 from its ASCII value to convert it to uppercase.
+			- If the character is an uppercase letter ('A' to 'Z'), add 32 to its ASCII value to convert it to lowercase.
+			- Write the toggled character to the standard output.
+			- Increment `i` to move to the next character.
+	- After all characters have been processed, print a newline (`'\n'`).
+	- Finally, return `0` to indicate successful execution.
+*/
 int main (int ac, char **av)
 {
 	if (ac == 2)
