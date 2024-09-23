520d7472a63484f654fbeefaf558b9ce3fd2c101
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:21:55 +0100

    Update ft_putstr.c

--- Lvl_1/ft_putstr/ft_putstr.c
+++ Lvl_1/ft_putstr/ft_putstr.c
@@ -1,5 +1,15 @@
 #include <unistd.h>
 
+/*
+	ft_putstr
+
+	This function takes a string as an argument and prints it to the standard output.
+
+	- We initialize an index `i` to 0 to track our position in the string.
+	- While the current character in the string is not null (meaning we haven't reached the end of the string):
+		- Write the character to the standard output;
+		- Move to the next character by incrementing `i`.
+*/
 void	ft_putstr(char *str)
 {
 	int i = 0;
