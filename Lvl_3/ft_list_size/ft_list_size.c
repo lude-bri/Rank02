709c8e45766c41c48a37ff3421ae07ad019998e2
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 20:02:21 +0100

    Update ft_list_size.c

--- Lvl_3/ft_list_size/ft_list_size.c
+++ Lvl_3/ft_list_size/ft_list_size.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+/*
+	ft_list_size
+
+	This function returns the size (i.e., the number of elements) of a linked list starting from `begin_list`.
+
+	- We initialize `i` to 0, which will count the number of nodes in the linked list.
+
+	- The function uses a `while` loop to traverse the linked list:
+		- The loop continues while `begin_list->next` is not `NULL`, meaning there are more nodes in the list.
+		- For each node, we increment `i` and move `begin_list` to the next node (`begin_list = begin_list->next`).
+
+	- When the loop finishes, `i` contains the number of nodes excluding the last one, since the loop terminates when `begin_list->next` is `NULL`.
+	- To return the correct count, the function should account for the last node by adding 1 to `i` before returning it.
+	
+	- If the list is empty (`begin_list == NULL`), the function will return 0.
+
+	- Finally, return the total number of nodes in the list.
+*/
 typedef struct s_list
 {
 	struct s_list *next;
