56f1d8826d869222885d9b7f88f03c90af556238
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:24:14 +0100

    Update ft_swap.c

--- Lvl_1/ft_swap/ft_swap.c
+++ Lvl_1/ft_swap/ft_swap.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+	ft_swap
+
+	This function swaps the values of two characters (pointers to char).
+
+	- We declare a temporary variable `tmp` to hold the value of `*a` (the value pointed to by `a`).
+	- Assign the value of `*b` to `*a`, effectively putting the value of `b` into `a`.
+	- Assign the value stored in `tmp` (originally `*a`) to `*b`, completing the swap.
+	- The function performs the swap without returning anything.
+*/
 char	ft_swap(char *a, char *b)
 {
 	char tmp;
