41b900bf3d2b1b6c5cc5c4cfdfdf767438b7d623
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 20:10:15 +0100

    Update pgcd.c

--- Lvl_3/pgcd/pgcd.c
+++ Lvl_3/pgcd/pgcd.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/*
+	pgcd (Greatest Common Divisor - GCD)
+
+	This function calculates the greatest common divisor (GCD) of two numbers `a` and `b` using the Euclidean algorithm.
+
+	- The Euclidean algorithm works as follows:
+		- If `b` divides `a` exactly (i.e., `a % b == 0`), then `b` is the GCD.
+		- Otherwise, the GCD of `a` and `b` is the same as the GCD of `b` and `a % b`.
+		- This process continues recursively until the remainder is zero, at which point the last non-zero remainder is the GCD.
+
+	- The function returns the GCD of `a` and `b`.
+*/
 unsigned int pgcd(unsigned int a, unsigned int b)
 {
 	if ((a % b) == 0)
