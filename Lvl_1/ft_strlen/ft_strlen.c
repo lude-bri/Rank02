075a0496fa98cfedc57c94385e5867f2ac224ed0
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:23:28 +0100

    Update ft_strlen.c

--- Lvl_1/ft_strlen/ft_strlen.c
+++ Lvl_1/ft_strlen/ft_strlen.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/*
+	ft_strlen
+
+	This function takes a string as an argument and returns the length of the string,
+	i.e., the number of characters before the null terminator.
+
+	- We initialize an index `i` to 0 to count the characters in the string.
+	- While the current character in the string is not null (meaning we haven't reached the end):
+		- Increment `i` to count each character.
+	- Once we've reached the null terminator, return the value of `i`, which now holds the length of the string.
+*/
 int	ft_strlen(char *str)
 {
 	int i = 0;
