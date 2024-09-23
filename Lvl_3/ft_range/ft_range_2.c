2e6744a2a9b272a11daf4b05956a2513f8bb039e
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 20:05:02 +0100

    Update ft_range_2.c

--- Lvl_3/ft_range/ft_range_2.c
+++ Lvl_3/ft_range/ft_range_2.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+/*
+	ft_range
+
+	This version of `ft_range` creates an array of integers that starts from `start` and ends at `end`.
+	The function dynamically allocates memory for the array and fills it with integers in increasing or decreasing order, based on the values of `start` and `end`.
+
+	- We initialize:
+		- `range` as the absolute difference between `start` and `end`.
+		- `arr` as a pointer to the dynamically allocated array with memory size equal to `sizeof(int) * range`.
+		- `i` as an index used to traverse and fill the array.
+
+	- The loop runs while `i` is less than `range`:
+		- Each iteration, we use the ternary operator to either increment or decrement `start` and assign its value to `arr[i]`.
+
+	- After the loop, we assign the final value of `start` to `arr[i]` to ensure the last value is included.
+
+	- The function returns the pointer to the array `arr`.
+*/
 int  abs(int nbr)
 {
   return ((nbr < 0) ? -nbr : nbr);
