20e0f2eb6f58219ea52344c7c42a1fdb484b50ac
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2024-09-23 11:37:56 +0100

    Update ft_strcspn.c

--- Lvl_2/ft_strcspn/ft_strcspn.c
+++ Lvl_2/ft_strcspn/ft_strcspn.c
@@ -1,5 +1,24 @@
 #include <unistd.h>
 
+/*
+	ft_strcspn
+
+	This function calculates the length of the initial segment of the string `s` that does not contain any characters from the string `reject`.
+	It returns the number of characters in `s` before encountering any character from `reject`.
+
+	- We initialize two variables:
+		- `len` to 0, which will track the length of the initial segment of `s` that doesn't contain any characters from `reject`.
+		- `i` to be used for iterating through the `reject` string.
+	
+	- The outer loop runs while the current character in `s[len]` is not null:
+		- For each character in `s`, we initialize `i` to 0 to start comparing it with the characters in `reject`.
+		- The inner loop runs through each character of `reject`:
+			- If the current character of `s[len]` matches any character in `reject[i]`, return the current value of `len` because this is where the match occurs.
+			- If no match is found in `reject`, increment `i` to check the next character in `reject`.
+		- If no character in `reject` matches `s[len]`, increment `len` to move to the next character in `s`.
+	
+	- If no character from `reject` is found in `s`, return the total length of the string `s` (i.e., the value of `len` when the loop finishes).
+*/
 size_t	ft_strcspn(const char *s, const char *reject)
 {
 	int i;
@@ -17,28 +36,4 @@ size_t	ft_strcspn(const char *s, const char *reject)
 		len++;
 	}
 	return (len);
-}
-
-size_t ft_strspn(const char *s, const char *accept)
-{
-	int i;
-	int j;
-	int check;
-
-	i = 0;
-	while (s[i])
-	{
-		j = 0;
-		check = 0;
-		while (accept[j])
-		{
-			if (s[i] == accept[j])
-				check = 1;
-			j++;
-		}
-		if (check == 0)
-			return (i);
-		i++;
-	}
-	return (i);
 }
